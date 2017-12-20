868abbfdad282ca349b3aa1bc025e9e8f1146ae0
Camille Franceschi <franceschi.camille@gmail.com>
2017-12-20 14:13:24 +0100

    added wave

--- src/components/landing.js
+++ src/components/landing.js
@@ -10,12 +10,12 @@ const Landing = () => {
     // backgroundImage: 'linear-gradient(210deg, rgba(57,212,216, 0.9) 10% , rgba(32,22,133, 1) 62%)'
     // backgroundImage: 'linear-gradient(-225deg, rgba(0,101,168,0.6) 0%, rgba(0,36,61,0.6) 50%)'
     // backgroundImage: 'linear-gradient( to bottom,#0F015E,rgb(57,212,216) )'
-    backgroundImage: 'radial-gradient(farthest-corner at 50% 100%, #772DFA,#3C2AE2)'
+    // backgroundImage: 'radial-gradient(farthest-corner at 50% 100%, #772DFA,#3C2AE2)'
 
   };
   return (
     <div>
-    <div style ={style}>
+    <div>
       <div className="banner">
         <div className="banner-content">
           <h1>Find the good place to go</h1>
@@ -23,19 +23,60 @@ const Landing = () => {
           <a className="btn-landing">DISCOVER PLACES</a>
         </div>
       </div>
-      <svg viewBox="0 0 1920 240" className="svg-wave"><title id="title">goop</title><g><path d="M1920,144.5l0,95.5l-1920,0l0,-65.5c196,-36 452.146,-15.726 657.5,8.5c229.698,27.098 870,57 1262.5,-38.5Z"></path></g></svg>
+
+      <div className="sc-cSHVUG fcASVK" data-reactid="37">
+        <div className="sc-fjdhpX khbfDq" data-reactid="38">
+          <svg className="sc-jzJRlG kfUdhq" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1720 240" data-reactid="39">
+            <g data-reactid="40"><path d="M1920,128.968l0,111.032l-1920,0l0,-73.337c23.928,-1.593 79.415,-0.702 234.999,-24.663c129.328,-19.917 303.543,0.636 504.699,24.367c344.472,40.638 767.952,90.597 1180.3,-37.399Z" data-reactid="41"></path></g>
+          </svg>
+        </div>
+        <div className="sc-bdVaJa iOgvef" data-reactid="42">
+          <div className="sc-htpNat dWdeUv" data-reactid="43">
+            <div className="sc-bwzfXH bXsnOD" data-reactid="44">
+              <h2 class="sc-kAzzGY hRZpIa" data-reactid="45">Don't let the top talents go away</h2>
+                <div class="sc-chPdSV fkZxUR" data-reactid="46">
+                  <div class="sc-kgoBCf jJgcuR" data-reactid="47">
+                    <h3 class="sc-kGXeez iBqPEE" data-reactid="48">Supercharge support</h3>
+                    <p class="sc-kpOJdX gjNgxa" data-reactid="49">Stop wasting time with endless private customer support threads answering the same question over and over.</p>
+                    <p class="sc-kpOJdX gjNgxa" data-reactid="50">Now your team can have conversations with your community as a whole and chat privately when a particular issue is sensitive.</p>
+                  </div>
+                  <div class="sc-kgoBCf cngWpI" data-reactid="51">
+                    <h3 class="sc-kGXeez iBqPEE" data-reactid="52">Bring people together</h3>
+                    <p class="sc-kpOJdX gjNgxa" data-reactid="53">Enlisted gives your top supporters and advocates a place to share their knowledge, empower others, and foster a place of belonging for everyone.</p>
+                  </div>
+                  <div class="sc-kgoBCf cngWpI" data-reactid="54">
+                    <h3 class="sc-kGXeez iBqPEE" data-reactid="55">Tighten your feedback loop</h3>
+                    <p class="sc-kpOJdX gjNgxa" data-reactid="56">There's no better feedback than the insights that come directly from your customers.</p>
+                    <p class="sc-kpOJdX gjNgxa" data-reactid="57">Think of Enlisted as a new direct line to discovering what your audience wants the most.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="sc-eNQAEJ kyEuTx" data-reactid="58">
+          <div className="sc-gZMcBi gDxRUg"></div>
+          <div className="sc-dxgOiQ dBTkFg" data-reactid="60">
+            <svg className="sc-ckVGcZ eUMwyW" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 240" data-reactid="61">
+              <g data-reactid="62"><path d="M1920,111.904l0,128.096l-1920,0l0,-132.536c390.745,104.115 1168.02,146.399 1763,34.536c60.63,-11.399 112.59,-21.405 157,-30.096Z" data-reactid="63"></path></g>
+            </svg>
+          </div>
+          <div class="sc-bdVaJa iOgvef" data-reactid="64">
+            <div class="sc-bwzfXH bXsnOD" data-reactid="65">
+              <div class="sc-kEYyzF kjSZfr" data-reactid="69">
+                <h2 class="sc-jKJlTe tgMkJ" data-reactid="70">An amazing solution</h2>
+                <p class="sc-kkGfuU UQFWb" data-reactid="71">Conversations on Enlisted are real-time chat, just like your favorite messaging app. But on Enlisted, conversations continue to provide value to more and more people over time.</p>
+                <p class="sc-kkGfuU UQFWb" data-reactid="72">Every conversation gets a unique link to make it easy for people to discover, share, or save for later.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      // <svg viewBox="0 0 1920 240" className="svg-wave"><title id="title">goop</title><g><path d="M1920,144.5l0,95.5l-1920,0l0,-65.5c196,-36 452.146,-15.726 657.5,8.5c229.698,27.098 870,57 1262.5,-38.5Z"></path></g></svg>
         {/* <svg  viewBox="0 0 1920 240" id="goop"><title id="title">goop</title><g><path d="M1920,144.5l0,95.5l-1920,0l0,-65.5c196,-36 452.146,-15.726 657.5,8.5c229.698,27.098 870,57 1262.5,-38.5Z"></path></g></svg> */}
         {/* <svg className="goop__InlineSvg-s6dcphf-0 GeMIC" fill-rule="evenodd" clip-rule="evenodd" xmlns="http://www.w3.org/2000/svg" aria-labelledby="title" viewBox="0 0 1920 240" id="goop"><title id="title">goop</title><g><path d="M1920,144.5l0,95.5l-1920,0l0,-65.5c196,-36 452.146,-15.726 657.5,8.5c229.698,27.098 870,57 1262.5,-38.5Z"></path></g></svg> */}
     </div>
-    <div>HEllo</div>
-    <div>HEllo</div>
-    <div>HEllo</div>
-    <div>HEllo</div>
-    <div>HEllo</div>
-    <div>HEllo</div>
-    <div>HEllo</div>
-    <div>HEllo</div>
-    <div>HEllo</div>
     </div>
 
 
